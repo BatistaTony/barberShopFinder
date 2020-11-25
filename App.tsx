bb679028f43bef5033a848a3d81fb57bbff85983
BatistaTony <batistatomeoliveira96@gmail.com>
2020-11-25 12:15:21 +0100

    app done

--- App.tsx
+++ App.tsx
@@ -9,37 +9,11 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import styled from 'styled-components/native';
-import ButtonHideCompletedTask from './components/buttonHide';
-import Header from './components/header';
-import TodoItem from './components/todo';
 
 const App: () => React.ReactNode = () => {
-  const todos = [
-    {
-      title: 'Dine',
-      isDone: false,
-    },
-    {
-      title: 'Beijoo',
-      isDone: true,
-    },
-  ];
-
   return (
     <MainContainer>
-      <Header />
-      <ButtonHideCompletedTask />
-
-      <ListTodo
-        data={todos}
-        renderItem={(itemData, index) => (
-          <TodoItem
-            key={index}
-            isDone={itemData.item.isDone}
-            title={itemData.item.title}
-          />
-        )}
-      />
+      <Text>Hello world App</Text>
     </MainContainer>
   );
 };
@@ -53,11 +27,6 @@ const MainContainer = styled.View`
   align-items: center;
   padding-left: 15px;
   padding-right: 15px;
-`;
-
-const ListTodo = styled.FlatList`
-  width: 100%;
-  margin-top: 25px;
 `;
 
 export default App;
